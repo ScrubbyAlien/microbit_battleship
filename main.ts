838483cc2b41e087543ffe6a58c93da177fe6e8c
pumpchief <58122434+pumpchief@users.noreply.github.com>
2020-09-21 14:29:12 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # # . .
+            . # # . .
+            . . . # .
+            . . . # #
+            . . . . #
+            `)
         basic.showString("Hello!")
     }
 })
