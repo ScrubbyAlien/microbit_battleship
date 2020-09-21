0d2e56bd28aa9cf0e566a5793ea75422723513cd
pumpchief <58122434+pumpchief@users.noreply.github.com>
2020-09-21 14:30:47 +0200

    removeklbjowihbn

--- main.ts
+++ main.ts
@@ -1,13 +1,5 @@
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 4; index++) {
-        basic.showLeds(`
-            # # # . .
-            . # # . .
-            . . . # .
-            . . . # #
-            . . . . #
-            `)
-        basic.showString("Hello!")
+    	
     }
 })
-basic.showString("Hej Axel! ")
