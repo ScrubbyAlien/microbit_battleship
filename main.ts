bbf081f5d674e40d7ad44f26fff3e9d965a7407b
pumpchief <58122434+pumpchief@users.noreply.github.com>
2020-09-21 14:39:02 +0200

    fixat allt vi klara ja

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
+let Bot: game.LedSprite = null
 input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 4; index++) {
-    	
+    for (let index = 0; index < 8; index++) {
+        Bot = game.createSprite(0, 0)
+        Bot.set(LedSpriteProperty.Y, randint(0, 4))
+        Bot.set(LedSpriteProperty.X, randint(0, 4))
     }
 })
